2f69d397f386b570c0bb05a5e97a789e052b8b1b
RosTobic <54326493+RosTobic@users.noreply.github.com>
2019-11-07 10:15:41 -0800

    Update import.py

--- import.py
+++ import.py
@@ -1,1 +1,21 @@
 (__name__) Markup("<option value=\"" + s + "\">" + s + "</option>")
+from flask import Flask, request, render_template. Markup, flash
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/aboutus") #annotations tell which function goes with which request
+def render_page1():
+    return render_template('page1.html')
+
+@app.route("/p2")
+def render_page2():
+    return render_template('page2.html')
+    
+if __name__=="__main__":
+    app.run(debug=False)
